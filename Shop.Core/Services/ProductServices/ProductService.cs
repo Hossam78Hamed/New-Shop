ce513ba08efe0efd5f41bd5c4316b34636cf0896
Hossam78Hamed <hossam78hamed@gmail.com>
2025-09-25 18:56:01 +0300

    Good

--- Shop.Core/Services/ProductServices/ProductService.cs
+++ Shop.Core/Services/ProductServices/ProductService.cs
@@ -21,9 +21,7 @@ namespace Shop.Core.Services.ProductServices
         private readonly IProductRepository productRepository;
         private readonly IPhotoService photoService;
         private readonly IMapper mapper;
-        
-        int x3 = 333;
-        int x4 = 44;
+
         public ProductService( IProductRepository _productRepository,IMapper _mapper,IPhotoService _photoService) {
             productRepository = _productRepository;
             photoService = _photoService;
